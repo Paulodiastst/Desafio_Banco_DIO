110bfd947378015728002cd353701d69b645aabc
Paulo Roberto <paulodiastst@hotmail.com>
2022-05-01 00:18:39 -0300

    Desafio Banco DIO

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import entities.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Menu chamaMenu = new Menu();
+
+        chamaMenu.menu();
+    }
+}
